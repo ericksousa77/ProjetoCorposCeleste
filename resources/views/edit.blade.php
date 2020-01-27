e1ecd81150d8fd23b9e3971432932775b069eea6
ericksousa77 <erickss@dcomp.ufs.br>
2020-01-27 19:07:34 -0300

    ajuste botao tela login

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -11,7 +11,7 @@
 
 @section('content')
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <h1>Editar Planeta</h1>
+    <h1>Listar Planeta</h1>
 
     <form action="{{ route('project.update')}}" method="POST">
         @method('put')
