cdb4335cc591a748dd38ae8ee8f47fc413203996
ericksousa77 <erickss@dcomp.ufs.br>
2020-01-27 19:17:26 -0300

    ajustando view index(list) e desfazendo pequenas alterações

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Deletar Corpo Celeste')
 
 <head>
-    <title>Editar</title>
+    <title>Listar</title>
     <style>
         .label-tam{width: 30%}
     </style>
@@ -11,7 +11,7 @@
 
 @section('content')
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <h1>Editar Planeta</h1>
+    <h1>Listar Planeta</h1>
 
 <form action="{{ route('project.update')}}" method="POST">
         @method('put')
@@ -24,7 +24,7 @@
 
         <p></p>
 
-        <button type="submit" class="btn btn-primary">Editar</button> 
+        <button type="submit" class="btn btn-primary">Listar</button> 
         <a href="{{route('principal') }}" class = "btn btn-primary">Voltar</a>
     </form>
 @endsection
