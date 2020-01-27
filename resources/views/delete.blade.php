eee1c1b9a3bcd39dabf80e467eb3a1f0a70367f7
ericksousa77 <erickss@dcomp.ufs.br>
2020-01-27 19:00:38 -0300

    voltando com middleware e ajuste de view

--- resources/views/delete.blade.php
+++ resources/views/delete.blade.php
@@ -7,21 +7,25 @@
     <style>
         .label-tam{width: 30%}
     </style>
+    <style> html, body {
+         background-color: #000;}
+    </style>
 </head>
 
 @section('content')
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <h1>Deletar Planeta</h1>
+         
 
 <form action="{{ route('project.destroy')}}" method="POST">
         @method('DELETE')
         @csrf
-
-        <div class="form-group label-tam">
-            <label for="formGroupExampleInput">Id do Planeta</label>
-            <input type="number" class="form-control" id="formGroupExampleInput" placeholder="ID">
+        <div align = center>
+            <div class="form-group label-tam ">  
+                <label for="formGroupExampleInput">Id do Planeta</label>
+                <input type="number" class="form-control" id="formGroupExampleInput" placeholder="ID">
+            </div>
         </div>
-
         <p></p>
 
         <button type="submit" class="btn btn-primary">Deletar</button> 
