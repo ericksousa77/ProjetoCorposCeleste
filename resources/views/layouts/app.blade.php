802cadfa46612cb204f82bb809b593fb4b51ef7d
ericksousa77 <erickss@dcomp.ufs.br>
2020-01-29 17:25:25 -0300

    ajustando titulo do login/register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Corpos Celestes</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
